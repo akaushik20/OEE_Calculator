0763d3cf17e4264d098970c7c1b10c7070502da8
Arpita Kaushik <arpita.k20@gmail.com>
2025-04-24 21:29:50 -0700

    commit

--- main.py
+++ main.py
@@ -11,7 +11,16 @@ def main():
     st.title("OEE Calculator")
     st.write("This tools help to calculate OEE (Overall Equipment Effectiveness)")
 
-    st.write("This is a simple example of a Streamlit app with a sidebar and main content area.")
+    # Input fields for OEE calculation
+    st.subheader("Input Data")
+    demand_annual = st.number_input("Demand Per Annum: ", min_value=0.0, max_value=3000000.0, value=30000.0, step=1.0)
+    production = st.number_input("Production Per Day: ", min_value=0.0, max_value=30000.0, value=240.0, step=0.1)
+    scheduled_time = st.number_input("Scheduled Time(3 shifts, 8 hours): ", min_value=0.0, max_value=30.0, value=20.0, step=0.1)
+    oee_loss = st.number_input("OEE Loss Target(Assumption) %: ", min_value=0.0, max_value=1.00, value=0.15, step=0.1)
+    
+    st.text("All Time: 24*60 = 1440 Minutes")
+
+    st.button("Calculate OEE", key="calculate_oee")
 
 if __name__ == "__main__":
     main()
